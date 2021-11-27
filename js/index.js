bedc72d17029aa7a1af6bee50390b76f4a327e0c
Haohan Jiang <haohan.jiang@nike.com>
2021-11-26 16:55:37 -0800

    simplified css

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const observer = new IntersectionObserver(entries => {
+const observer_intro = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         const intro_div = entry.target.querySelector('.intro_text');
 
@@ -11,4 +11,22 @@ const observer = new IntersectionObserver(entries => {
     });
 });
 
-observer.observe(document.querySelector('.parallax-intro'));
+observer_intro.observe(document.querySelector('.parallax-intro'));
+
+// Remove the transition class
+const square = document.querySelectorAll('.card');
+square.forEach(card => {
+    card.classList.remove('card-transition');
+
+    const observer_work = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                card.classList.add('card-transition');
+                return;
+            }
+
+            card.classList.remove('card-transition');
+        });
+    });
+    observer_work.observe(document.querySelector('.card_container'));
+})
