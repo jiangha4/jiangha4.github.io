3f9bce0b6f994dacc913d166a05c69e72ca9d1e8
Haohan Jiang <haohan.jiang@nike.com>
2021-11-27 11:09:52 -0800

    uoft card content

--- js/index.js
+++ js/index.js
@@ -1,17 +1,3 @@
-//Get the button:
-mybutton = document.getElementsByClassName("topBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scroll()};
-
-function scroll() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
-}
-
 // When the user clicks on the button, scroll to the top of the document
 function topOfPage() {
     document.body.scrollTop = 0; // For Safari
